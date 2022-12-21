d8ed7e5478b88c1e0a1f606bf425bc9f0d373180
sadman-shami <srahyme413@gmail.com>
2022-12-21 19:45:42 +0600

    Refactor TodoContext.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
 import { VStack } from "@chakra-ui/react";
-import React, { useEffect, useReducer } from "react";
+import React from "react";
 import TodoHeader from "./components/TodoHeader";
 import TodoInput from "./components/TodoInput";
 import Todos from "./components/Todos";
 import TodoProvider from "./context/TodoContext";
-import { initialValue, reducer } from "./reducer";
 
 const App = () => {
-  const [todos, dispatch] = useReducer(reducer, initialValue);
-  useEffect(() => {
-    if (!localStorage.getItem("todos")) {
-      localStorage.setItem("todos", JSON.stringify([]));
-    } else {
-      const todos = JSON.parse(localStorage.getItem("todos"));
-      dispatch({
-        type: "getTodo",
-        data: [...todos],
-      });
-    }
-  }, []);
-
   return (
     <React.Fragment>
-      <TodoProvider data={{ todos: todos.todos, dispatch }}>
+      <TodoProvider>
         <VStack padding={6} height="100vh" gap={10}>
           <TodoHeader />
           <Todos />
--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -1,10 +1,24 @@
-import React, { createContext } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
+import { initialValue, reducer } from "../reducer";
 
 const TodoContext = createContext();
 
 const TodoProvider = (props) => {
+  const [todos, dispatch] = useReducer(reducer, initialValue);
+  useEffect(() => {
+    if (!localStorage.getItem("todos")) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      const todos = JSON.parse(localStorage.getItem("todos"));
+      dispatch({
+        type: "getTodo",
+        data: [...todos],
+      });
+    }
+  }, []);
+
   return (
-    <TodoContext.Provider value={props.data} {...props}>
+    <TodoContext.Provider value={{ todos: todos.todos, dispatch }}>
       {props.children}
     </TodoContext.Provider>
   );
